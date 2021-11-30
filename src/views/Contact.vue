9ea80f3a63f300e8f115297923e70d8f9c7b4e13
Andrew <r04631044@ntu.edu.tw>
2021-11-30 22:29:36 +0800

    fix joinpcpog

---
+++ src/views/Contact.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <div>
+
+    5566
+       
+     
+    </div>
+</template>
+<style >
+
+</style>
--- src/views/JoinPcpogo.vue
+++ src/views/JoinPcpogo.vue
@@ -6,7 +6,7 @@
             <Steps :model="items" :readonly="true" />
         </div>
 
-        <router-view v-slot="{Component}" :formData="formObject" @prevPage="prevPage($event)" @nextPage="nextPage($event)" @complete="complete">
+        <router-view v-slot="{ Component }">
             <keep-alive>
                 <component :is="Component" />
             </keep-alive>
@@ -16,48 +16,20 @@
 
 <script>
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { useToast } from 'primevue/usetoast';
 
 export default {
     setup() {
-        const router = useRouter();
-        const toast = useToast();
+
+
         const items = ref([
             {
                 label: 'Contact',
-                to: "/Contact"
+                to: "/"
             },
-            {
-                label: 'Seat',
-                to: "/seat",
-            },
-            {
-                label: 'Payment',
-                to: "/payment",
-            },
-            {
-                label: 'Confirmation',
-                to: "/confirmation",
-            }
+           
         ]);
-        const formObject = ref({});
-
-        const nextPage = (event) => {
-            for (let field in event.formData) {
-                formObject.value[field] = event.formData[field];
-            }
-
-            router.push(items.value[event.pageIndex + 1].to);
-        };
-        const prevPage = (event) => {
-            router.push(items.value[event.pageIndex - 1].to);
-        };
-        const complete = () => {
-            toast.add({severity:'success', summary:'Order submitted', detail: 'Dear, ' + formObject.value.firstname + ' ' + formObject.value.lastname + ' your order completed.'});
-        };
-
-        return { items, formObject, nextPage, prevPage, complete }
+        
+        return { items }
     }
 }
 </script>
